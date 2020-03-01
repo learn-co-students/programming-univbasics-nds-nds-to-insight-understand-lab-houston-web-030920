f951a680a28375f4543e10d5e92fce8bd29aae31
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-03-01 20:19:27 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,10 +8,10 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  i=0 
+  count=0 
   movies = directors_database[0][:movies]
-  while i < movies.count do 
-    puts movies [i][:title]
-    i += 1
+  while count < movies.count do 
+    puts movies [count][:title]
+    count += 1
 end 
 end
